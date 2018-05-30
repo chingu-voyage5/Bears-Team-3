6836291689e0c76835c9f3f316ee4abd03c06b23
Vered Rekanati <veredrec@gmail.com>
2018-05-30 06:44:29 -0400

    Add initial vue setup for the front end Relates #2

---
+++ bears-3/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+    <h2>Bears-Team-3 :)</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

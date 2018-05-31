e291fb8f89d110591739bf21dc3a7e83d9df9dd8
ElleeB <laura.trager@gmail.com>
2018-05-30 21:51:27 -0400

    Update with Postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_124443) do
+ActiveRecord::Schema.define(version: 2018_05_30_220407) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "counters", force: :cascade do |t|
     t.text "number_count"

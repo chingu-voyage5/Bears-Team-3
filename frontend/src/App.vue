e311680007d2fe25c0a88c4f9161d73ce9c5d824
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-06-09 18:00:13 -0400

    Increase number count on button click

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@
     <h1>Hello World</h1>
     <h2>Bears-Team-3 :)</h2>
     <p>{{ currentCount }}</p>
+    <button v-on:click.prevent="onIncreaseClick">Increase Counter</button>
   </div>
 </template>
 
@@ -13,6 +14,13 @@ export default {
     return {
       currentCount: 0,
     };
+  },
+  methods: {
+    onIncreaseClick() {
+      const headers = { 'content-type': 'application/json' };
+      const options = { method: 'POST', headers };
+      fetch('/api/counters', options).then(res => res.json()).then(newCount => this.currentCount = newCount.number_count);
+    }
   },
   mounted() {
     fetch('/api/counters').then(res => res.json()).then(count => this.currentCount = count.number_count);

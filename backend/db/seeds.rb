b3b1b73161946e2ba0ecd9a1e0cc4b546ce635eb
ElleeB <laura.trager@gmail.com>
2018-06-18 13:09:52 -0400

    Add seed data to seeds.rb file round 1

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -38,7 +38,103 @@ Organization.create(
   info_email: "donations@bbbs.org"
 )
 
+Organization.create (
+  name: "Community Foundation of St. Joseph County",
+  address_1: "205 W. Jefferson Blvd.",
+  address_2: "Suite 610",
+  city: "South Bend",
+  state: "Indiana",
+  zip: "46624",
+  phone: "574-232-0041",
+  blurb: "The Community Foundation of St. Joseph County is a charitable endowment that improves the quality of life for the citizens of St. Joseph County and their succeeding generations. We receive gifts from people who care about the St. Joseph County community, we invest these gifts forever, and we use the investment earnings to make grants each year to local charities.",
+  url: "http://cfsjc.org/",
+  donate_url: "http://cfsjc.org/for-donors/give-now/",
+  info_email: "info@cfsjc.org",
+)
 
+Organization.create (
+  name: "HIAS",
+  address_1: "300 Spring Street",
+  address_2: "Suite 500",
+  city: "Silver Spring",
+  state: "Maryland",
+  zip: "20910",
+  phone: "301-844-7300",
+  blurb: "HIAS stands for a world in which refugees find welcome, safety, and freedom. HIAS rescues people whose lives are in danger for being who they are. We protect the most vulnerable refugees, helping them build new lives and reuniting them with their families in safety and freedom. We advocate for the protection of refugees and assure that displaced people are treated with the dignity they deserve. Guided by our Jewish values and history, we bring more than 130 years of expertise to our work with refugees.",
+  url: "https://www.hias.org/",
+  donate_url: "https://act.hias.org/page/5056/donate/1",
+  info_email: "",
+)
+
+Organization.create (
+  name: "Sunrise USA",
+  address_1: "P.O. Box 250549",
+  address_2: "",
+  city: " Franklin",
+  state: "Michigan",
+  zip: "48025",
+  phone: "855-797- 3863",
+  blurb: "Founded in 2011 by a group of Syrian-American professionals, Sunrise-USA a 501 (c) 3 Organization is now one of the leading NGO’s in the United states focused on providing humanitarian assistance to Syrians both inside Syria and in neighboring countries.",
+  url: "https://sunrise-usa.org/",
+  donate_url: "https://sunrise-usa.org/get-involved/donate-online/",
+  info_email: "info@ssfusa.org"
+)
+
+Organization.create (
+  name: "The Albert Einstein Institution",
+  address_1: "P.O. Box 455",
+  address_2: "",
+  city: "East Boston",
+  state: "Massachusetts",
+  zip: "02128",
+  phone: "",
+  blurb: "The mission of the Albert Einstein Institution is to advance the worldwide study and strategic use of nonviolent action in conflict. The Institution is committed to: defending democratic freedoms and institutions; opposing oppression, dictatorship, and genocide; and reducing the reliance on violence as an instrument of policy.",
+  url: "https://www.aeinstein.org/",
+  donate_url: "https://www.paypal.com/donate/?token=uuekuZfM3IKQ6KcBeuy0v3FryktHy_nbj-TCoiOJLZtSnaUnlFk8ApiJA9DncadS43k4uW&country.x=US&locale.x=US",
+  info_email: " info@aeinstein.org  "
+)
+
+Organization.create (
+  name: "Code.org",
+  address_1: "1501 4th Ave",
+  address_2: "Suite 900",
+  city: "Seattle",
+  state: "Washington",
+  zip: "98101",
+  phone: "",
+  blurb: "Code.org® is a nonprofit dedicated to expanding access to computer science in schools and increasing participation by women and underrepresented minorities. Our vision is that every student in every school has the opportunity to learn computer science, just like biology, chemistry or algebra. Code.org provides the leading curriculum for K-12 computer science in the largest school districts in the United States and Code.org also organizes the annual Hour of Code campaign which has engaged 10% of all students in the world. Code.org is supported by generous donors including Amazon, Facebook, Google, the Infosys Foundation, Microsoft, and many more.",
+  url: "https://code.org/",
+  donate_url: "https://donate.code.org/campaign/computer-science-education/c142257",
+  info_email: "info@code.org"
+)
+
+Organization.create (
+  name: "Water for people",
+  address_1: "100 E. Tennessee Ave. ",
+  address_2: "",
+  city: "Denver",
+  state: "Colorado",
+  zip: "80209",
+  phone: "720-488-4590",
+  blurb: "Water For People exists to promote the development of high-quality drinking water and sanitation services, accessible to all, and sustained by strong communities, businesses, and governments.",
+  url: "https://www.waterforpeople.org/",
+  donate_url: "https://www.waterforpeople.org/donate",
+  info_email: "info@waterforpeople.org"
+)
+
+# Organization.create (
+#   name: "",
+#   address_1: "",
+#   address_2: "",
+#   city: "",
+#   state: "",
+#   zip: "",
+#   phone: "",
+#   blurb: "",
+#   url: "",
+#   donate_url: "",
+#   info_email: ""
+# )
 
 
 # Organization.create (

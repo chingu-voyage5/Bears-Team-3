c5960329e1f032228750b03b6213bfc1c22ed045
ElleeB <laura.trager@gmail.com>
2018-06-18 09:38:16 -0400

    Add non-profit db migration

--- backend/app/models/counter.rb
+++
@@ -1,6 +0,0 @@
-class Counter < ApplicationRecord
-  def increase_count
-    self.update(number_count: self.number_count + 1)
-    self
-  end
-end
--- backend/db/migrate/20180604143221_create_counters.rb
+++
@@ -1,7 +0,0 @@
-class CreateCounters < ActiveRecord::Migration[5.2]
-  def change
-    create_table :counters do |t|
-      t.text :number_count
-    end
-  end
-end

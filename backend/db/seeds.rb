9f6bede7a3793bec723bcb109d5df1e89fced272
ElleeB <laura.trager@gmail.com>
2018-06-18 21:32:21 -0400

    Add seed data to seeds.rb file round 2

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -19,6 +19,7 @@ Organization.create(
   zip: "08558",
   phone: "609-309-5214",
   blurb: "Founded in 1941, SAVE is an independent non-profit animal shelter dedicated to protecting the health and welfare of homeless companion animals in the greater Princeton area. Through six core programs of Rescue, Shelter, Health and Welfare, Spay/Neuter, Adoption, and Humane Education, SAVE focuses on the rehabilitation and successful placement of treatable and adoptable animals. SAVE endeavors to build, foster, and strengthen the human-animal bond.",
+  about: ""
   url: "https://savehomelessanimals.org/",
   donate_url: "https://savehomelessanimals.org/donate/",
   info_email: "save@savehomelessanimals.org",
@@ -34,6 +35,7 @@ Organization.create(
   zip: "33607",
   phone: "813-720-8778",
   blurb: "Big Brothers Big Sisters helps children realize their potential and build their futures. We nurture children and strengthen communities. And we couldn't do any of it without you.",
+  about: ""
   url: "http://www.bbbs.org/",
   donate_url: "https://secured.bbbs.org/ea-action/action?ea.campaign.id=57589&ea.client.id=2000&_ga=2.110321989.1929660107.1529339387-829018715.1529339387",
   info_email: "donations@bbbs.org",
@@ -49,6 +51,7 @@ Organization.create (
   zip: "46624",
   phone: "574-232-0041",
   blurb: "The Community Foundation of St. Joseph County is a charitable endowment that improves the quality of life for the citizens of St. Joseph County and their succeeding generations. We receive gifts from people who care about the St. Joseph County community, we invest these gifts forever, and we use the investment earnings to make grants each year to local charities.",
+  about: ""
   url: "http://cfsjc.org/",
   donate_url: "http://cfsjc.org/for-donors/give-now/",
   info_email: "info@cfsjc.org",
@@ -63,7 +66,8 @@ Organization.create (
   state: "Maryland",
   zip: "20910",
   phone: "301-844-7300",
-  blurb: "HIAS stands for a world in which refugees find welcome, safety, and freedom. HIAS rescues people whose lives are in danger for being who they are. We protect the most vulnerable refugees, helping them build new lives and reuniting them with their families in safety and freedom. We advocate for the protection of refugees and assure that displaced people are treated with the dignity they deserve. Guided by our Jewish values and history, we bring more than 130 years of expertise to our work with refugees.",
+  blurb: "HIAS stands for a world in which refugees find welcome, safety, and freedom. HIAS rescues people whose lives are in danger for being who they are. We protect the most vulnerable refugees, helping them build new lives and reuniting them with their families in safety and freedom. We advocate for the protection of refugees and assure that displaced people are treated with the dignity they deserve. Guided by our Jewish values and history, we bring more than 130 years of expertise to our
+  about: "" work with refugees.",
   url: "https://www.hias.org/",
   donate_url: "https://act.hias.org/page/5056/donate/1",
   info_email: "",
@@ -79,6 +83,7 @@ Organization.create (
   zip: "48025",
   phone: "855-797- 3863",
   blurb: "Founded in 2011 by a group of Syrian-American professionals, Sunrise-USA a 501 (c) 3 Organization is now one of the leading NGO’s in the United states focused on providing humanitarian assistance to Syrians both inside Syria and in neighboring countries.",
+  about: ""
   url: "https://sunrise-usa.org/",
   donate_url: "https://sunrise-usa.org/get-involved/donate-online/",
   info_email: "info@ssfusa.org",
@@ -94,6 +99,7 @@ Organization.create (
   zip: "02128",
   phone: "",
   blurb: "The mission of the Albert Einstein Institution is to advance the worldwide study and strategic use of nonviolent action in conflict. The Institution is committed to: defending democratic freedoms and institutions; opposing oppression, dictatorship, and genocide; and reducing the reliance on violence as an instrument of policy.",
+  about: ""
   url: "https://www.aeinstein.org/",
   donate_url: "https://www.paypal.com/donate/?token=uuekuZfM3IKQ6KcBeuy0v3FryktHy_nbj-TCoiOJLZtSnaUnlFk8ApiJA9DncadS43k4uW&country.x=US&locale.x=US",
   info_email: " info@aeinstein.org",
@@ -108,7 +114,8 @@ Organization.create (
   state: "Washington",
   zip: "98101",
   phone: "",
-  blurb: "Code.org® is a nonprofit dedicated to expanding access to computer science in schools and increasing participation by women and underrepresented minorities. Our vision is that every student in every school has the opportunity to learn computer science, just like biology, chemistry or algebra. Code.org provides the leading curriculum for K-12 computer science in the largest school districts in the United States and Code.org also organizes the annual Hour of Code campaign which has engaged 10% of all students in the world. Code.org is supported by generous donors including Amazon, Facebook, Google, the Infosys Foundation, Microsoft, and many more.",
+  blurb: "Code.org® is a nonprofit dedicated to expanding access to computer science in schools and increasing participation by women and underrepresented minorities. Our vision is that every student in every school has the opportunity to learn computer science, just like biology, chemistry or algebra. Code.org provides the leading curriculum for K-12 computer science in the largest school districts in the United States and Code.org also organizes the annual Hour of Code campaign which has
+  about: "" engaged 10% of all students in the world. Code.org is supported by generous donors including Amazon, Facebook, Google, the Infosys Foundation, Microsoft, and many more.",
   url: "https://code.org/",
   donate_url: "https://donate.code.org/campaign/computer-science-education/c142257",
   info_email: "info@code.org",
@@ -124,6 +131,7 @@ Organization.create (
   zip: "80209",
   phone: "720-488-4590",
   blurb: "Water For People exists to promote the development of high-quality drinking water and sanitation services, accessible to all, and sustained by strong communities, businesses, and governments.",
+  about: ""
   url: "https://www.waterforpeople.org/",
   donate_url: "https://www.waterforpeople.org/donate",
   info_email: "info@waterforpeople.org",
@@ -139,12 +147,157 @@ Organization.create (
   zip: "10460",
   phone: "718-220-5100",
   blurb: "WCS's goal is to conserve the world's largest wild places in 16 priority regions, home to more than 50% of the world's biodiversity.",
+  about: ""
   url: "https://www.wcs.org/",
   donate_url: "https://www.wcs.org/support",
   info_email: "",
   size: "Large"
 )
 
+Organization.create (
+  name: "http://brilliantdetroit.org/",
+  address_1: "5675 Larkins Street",
+  address_2: "",
+  city: "Detroit",
+  state: "Michigan",
+  zip: "48210",
+  phone: "313-483-8992",
+  blurb: "Brilliant Detroit is dedicated to building kid success families and neighborhoods where families with children 0-8 have what they need to be school ready, healthy and stable. ",
+  about: ""
+  url: "http://brilliantdetroit.org/",
+  donate_url: "http://brilliantdetroit.org/donate/",
+  info_email: "info@brilliantdetroit.org",
+  size: "Small"
+)
+
+Organization.create (
+  name: "Jewish World Services",
+  address_1: "45 West 36th Street",
+  address_2: "",
+  city: "New York",
+  state: "New York",
+  zip: "10018",
+  phone: "800.889.7146",
+  blurb: "Inspired by the Jewish commitment to justice, American Jewish World Service (AJWS) works to realize human rights and end poverty in the developing world.",
+  about: ""
+  url: "https://ajws.org/",
+  donate_url: "https://ajws.org/donate/default/",
+  info_email: "ajws@ajws.org",
+  size: "Large"
+)
+
+Organization.create (
+  name: "UNHCR",
+  address_1: "",
+  address_2: "",
+  city: "",
+  state: "",
+  zip: "",
+  phone: "855-808-6427",
+  blurb: "UNHCR helps to save, protect and rebuild the lives of millions of refugees and others forced to flee their homes due to violence and conflict.",
+  about: ""
+  url: "https://www.unrefugees.org/",
+  donate_url: "https://give.unrefugees.org/180117CORE_MAINPG_P_3000/?_ga=2.153040127.731079319.1529343487-2007167379.1529153210&_gac=1.90784232.1529343487.CjwKCAjwsJ3ZBRBJEiwAtuvtlDuSQhPfqZDuSbDa8XVnU1c5syK8LLanp3FUITiVnlq2b-wOxdZ7CxoCM_wQAvD_BwE",
+  info_email: "",
+  size: "Large"
+)
+
+Organization.create (
+  name: "Malala Fund",
+  address_1: "PO Box 53347",
+  address_2: "",
+  city: "Washington",
+  state: "D.C.",
+  zip: "20009",
+  phone: "",
+  blurb: "Malala Fund is working for a world where every girl can learn and lead without fear.",
+  about: ""
+  url: "https://www.malala.org/",
+  donate_url: "https://www.classy.org/give/154885/#!/donation/checkout",
+  info_email: "",
+  size: "Large"
+)
+
+
+Organization.create (
+  name: "The Nature Conservancy",
+  address_1: "4245 North Fairfax Drive",
+  address_2: "Suite 100",
+  city: "Arlington",
+  state: "Virginia",
+  zip: "22203",
+  phone: "800-628-6860",
+  blurb: "Stand up for our natural world with The Nature Conservancy. Every acre we protect, every river mile restored, every species brought back from the brink, begins with you.",
+  about: ""
+  url: "https://www.nature.org/",
+  donate_url: "https://support.nature.org/site/Donation2?10420.donation=form1&df_id=10420&intc=nature.tnav.button.donate&ben=mag",
+  info_email: "",
+  size: "Large"
+)
+
+Organization.create (
+  name: "Anti Defamation League",
+  address_1: "",
+  address_2: "",
+  city: "",
+  state: "",
+  zip: "",
+  phone: "",
+  blurb: "We want an ever-more just society. We continually develop new programs, policies and skills to expose and combat whatever holds us back.",
+  about: ""
+  url: "https://www.adl.org/",
+  donate_url: "https://www.classy.org/give/174715/#!/donation/checkout",
+  info_email: "https://www.adl.org/contact",
+  size: "Large"
+)
+
+Organization.create (
+  name: "Southern Poverty Law Center",
+  address_1: "400 Washington Ave.",
+  address_2: "",
+  city: "Montgomery",
+  state: "Alabama",
+  zip: "36104",
+  phone: "334-956-8200",
+  blurb: "The Southern Poverty Law Center is dedicated to fighting hate and bigotry and to seeking justice for the most vulnerable members of our society.",
+  about: ""
+  url: "https://www.splcenter.org/",
+  donate_url: "https://donate.splcenter.org/sslpage.aspx?pid=463",
+  info_email: "",
+  size: "Large"
+)
+
+Organization.create (
+  name: "Wild Wings",
+  address_1: "",
+  address_2: "",
+  city: "Hazel Park",
+  state: "Michigan",
+  zip: "48030",
+  phone: "248-701-2523",
+  blurb: "Songbird and crow rehabilitation",
+  about: ""
+  url: "http://wildwingsbirdrehab.org/",
+  donate_url: "https://www.paypal.com/donate/?token=b1pleeP0QTGeIlpKdbdL_ZX_V0FKA8pC2nJWIxmnJ6nGWMiQ_nUHftxRwYU4qsY91ccYDG&country.x=US&locale.x=US",
+  info_email: "WWbirdrehab@comcast.net",
+  size: "Small"
+)
+
+Organization.create (
+  name: "Food & Water Watch",
+  address_1: "1616 P Street NW",
+  address_2: "",
+  city: "Washington",
+  state: "D.C.",
+  zip: "20036",
+  phone: "202-683-2500",
+  blurb: "Food & Water Watch champions healthy food and clean water for all.",
+  about: ""
+  url: "https://www.foodandwaterwatch.org/",
+  donate_url: "https://secure.foodandwaterwatch.org/donate/donate-food-water-watch-0",
+  info_email: "info@fwwatch.org",
+  size: ""
+)
 
 # Organization.create (
 #   name: "",
@@ -155,7 +308,9 @@ Organization.create (
 #   zip: "",
 #   phone: "",
 #   blurb: "",
+    # about: ""
 #   url: "",
 #   donate_url: "",
 #   info_email: "",
+#   size: "
 # )

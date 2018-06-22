16315c269cc125924cbd629bfab0ad366b95664c
Vered Rekanati <veredrec@gmail.com>
2018-06-22 08:00:48 -0400

    Setup router with basic header, footer, and pages Relates #24

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Header from './components/Header/Header.vue';
-// import Footer from './components/Footer/Footer.vue';
+import Footer from './components/Footer/Footer.vue';
 
 Vue.component('app-header', Header);
-// Vue.component('app-footer', Footer);
+Vue.component('app-footer', Footer);
 
 new Vue({
   el: '#app',

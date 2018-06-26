58645916243603c0f05a03349349d04b5e0869c2
ElleeB <laura.trager@gmail.com>
2018-06-26 11:35:29 -0400

    Add organization#index route and view

--- backend/app/controllers/organizations_controller.rb
+++ backend/app/controllers/organizations_controller.rb
@@ -1,5 +1,10 @@
 class OrganizationsController < ApplicationController
 
+  def index
+    @organizations = Organization.all
+    # render json: @organizations
+  end
+
   def show
     @organization = Organization.find(params[:id])
     render json: @organization

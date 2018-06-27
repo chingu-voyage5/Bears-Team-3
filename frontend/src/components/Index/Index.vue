092d04128bd47e08a0ea73e42a2723b44fe5ecc4
Vered Rekanati <veredrec@gmail.com>
2018-06-27 06:27:43 -0400

    Add initial index page to browse the organizations Relates #7

---
+++ frontend/src/components/Index/Index.vue
@@ -0,0 +1,7 @@
+<template src="./Index.html"></template>
+
+<style scoped lang="css">
+  @import 'Index.css'
+</style>
+
+<script></script>
--- frontend/src/components/List/List.vue
+++
@@ -1,7 +0,0 @@
-<template src="./List.html"></template>
-
-<style scoped lang="css">
-  @import 'List.css'
-</style>
-
-<script></script>

42c2df1f517d28be9fa2f1ca4cefcc7521e14542
ElleeB <laura.trager@gmail.com>
2018-07-13 16:32:30 -0400

    Remove 'pry' and add 'csv' gems

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_18_134208) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "organizations", force: :cascade do |t|
     t.string "name"
     t.string "address_1"

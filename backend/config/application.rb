1068b2432d6c61dd0f27744a4578d3322ebf9f04
ElleeB <laura.trager@gmail.com>
2018-07-13 20:37:21 -0400

    Add load_tasks to application.rb

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,16 +1,23 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Explore options for initialization including run rake task import:organizations
+# Rails.load_config_initializers
+# Rake::Task['import:organizations'].execute
+
 module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    require 'rake'
+    load_tasks
+
+    Rake::Task['import:organizations'].invoke
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

ce8fc8ca2de4d7070e63df1957d2e07d047b48b6
ElleeB <laura.trager@gmail.com>
2018-07-18 21:43:22 -0400

    Migrate organizations table success

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,8 +1,5 @@
 require_relative 'boot'
 require 'rails/all'
-# Rake::Task['import:organizations'].invoke
-
-# Rake("import:organizations")
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,19 +9,6 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-
-  ###
-  # rake = Rake.application
-  # rake.init
-  # you can import addition *.rake files
-  # app.add_import 'some/other/file.rake'
-  # rake.load_rakefile
-  (rake['db:create'].invoke "testapp_development")
-  #=> Database 'testapp_development' already exists
-  #=> Database 'testapp_test' already exists
-  ###
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- backend/config/initializers/db_prep.rb
+++
@@ -1,8 +0,0 @@
-require 'rake'
-
-Rake::Task['db:drop']
-Rake::Task['db:create']
-Rake::Task['db:migrate']
-
-Rails.application.load_tasks # <---
-Rake::Task['import:organizations'].invoke

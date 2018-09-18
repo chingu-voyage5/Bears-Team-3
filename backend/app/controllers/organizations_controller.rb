785265b5366d86661de8b93e8d5c48828c26ea53
ElleeB <laura.trager@gmail.com>
2018-09-17 21:30:35 -0400

    Add organizations/new form and link to show

--- backend/app/controllers/organizations_controller.rb
+++ backend/app/controllers/organizations_controller.rb
@@ -10,11 +10,26 @@ class OrganizationsController < ApplicationController
   end
 
   def create
+    if @organization = Organization.create(organization_params)
+      session[:user_id] = @user.id
+
+      # ActiveRecord validation errors can be captured here
+      redirect_to organization_path(@organization)
+    else
+
+      render :new
+    end
   end
 
   def show
     @organization = Organization.find(params[:id])
     # render json: @organization
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:name, :address_1, :address_2, :city, :state, :zip, :phone, :blurb, :about, :url, :donate_url, :info_email, :size, :image, :user_id, user: [:username, :password, :password_confirmation])
   end
 
 end

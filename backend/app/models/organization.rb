4f70f2d66b43bce8971dcc5f92eeec023f4163ef
ElleeB <laura.trager@gmail.com>
2018-09-17 20:32:18 -0400

    Add user_id column to organizations to initiate user admin

--- backend/app/models/organization.rb
+++ backend/app/models/organization.rb
@@ -2,6 +2,6 @@ class Organization < ActiveRecord::Base
   has_secure_password
   # has_many :donations
   # has_many :donors, through: :donations
-  # has_many :oragnizations_causes
+  # has_many :organizations_causes
   # has_many :causes, through: organizations_causes
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true
-  # validates :email, format: { with: "/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" }
+  # would like to validate via verification email to user
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { in: 6..20 }
 end

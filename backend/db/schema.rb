623b07087dab1d0e3e3935db713b2505f4e7a5c4
ElleeB <laura.trager@gmail.com>
2018-09-17 20:33:25 -0400

    Migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_002436) do
+ActiveRecord::Schema.define(version: 2018_09_18_003019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_002436) do
     t.string "size"
     t.string "image"
     t.string "password_digest"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
